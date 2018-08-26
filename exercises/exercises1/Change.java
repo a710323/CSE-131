04503e344aaa6520e9031547b71ea153395edbb2
alexteng <alexteng@192.168.1.8>
2018-08-26 00:18:31 -0500

    Firstly, I used Scanner to get the input from user. But I realized that there is a method defined by professor so I changed to ArgsProcessor. I added the check and if/else statement after I saw the professor's solution video. That's my bad habit, usually forget the check the result and just hit the run button.

--- exercises/exercises1/Change.java
+++ exercises/exercises1/Change.java
@@ -1,5 +1,7 @@
 package exercises1;
 
+import java.util.Scanner;
+
 import cse131.ArgsProcessor;
 
 public class Change {
@@ -8,14 +10,42 @@ public class Change {
 		//
 		// Below, prompt the user to enter a number of pennies
 		//
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int initialPennies = ap.nextInt("How many pennies?");
+		//System.out.println("The pennies you have is: " + initialPennies);
+		int dollar = initialPennies / 100;
+		int penniesAfterDollar = initialPennies % 100;
+		//System.out.println("You have " + dollar + " dollars");
+		int quarter = penniesAfterDollar / 25;
+		int penniesAfterQuarter = penniesAfterDollar % 25;
+		//System.out.println("And " + quarter + " quarters");
+		int dime = penniesAfterQuarter / 10;
+		int penniesAfterDime = penniesAfterQuarter % 10;
+		//System.out.println("And " + dime + " dimes");
+		int pickle = penniesAfterDime / 5;
+		int penniesAfterPickle = penniesAfterDime % 5;
+		//System.out.println("And " + pickle + " pickles");
+		//System.out.println("And " + penniesAfterPickle);
+		System.out.printf("In the end, you possess: %n%d dollar%n%d quarter%n%d dime%n%d pickle%n%d pennies%n", dollar, quarter, dime, pickle, penniesAfterPickle);
 		
+		int check = penniesAfterPickle + pickle * 5 + dime * 10 + quarter * 25 + dollar * 100;
+		if(check == initialPennies) {
+			System.out.println("nThe system is ready to operate.");
+		}
+		else {
+			System.out.println("There are some bugs in the system, correct it!");
+		}
 		
+	
+	
+		
+
 		//
 		// Then, compute and print out how many 
 		//    dollars, quarters, dimes, nickels, and pennies
 		// should be given in exchange for those pennies, so as to
 		// minimize the number of coins (see the videos)
 		//
-	}
-
+	
+}
 }
