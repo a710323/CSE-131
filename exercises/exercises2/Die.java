3451666b5deaa1e36340fa1a71c4052530fe57d6
alexteng <alexteng@192.168.1.8>
2018-08-29 18:37:07 -0500

    exercise2 die done

--- exercises/exercises2/Die.java
+++ exercises/exercises2/Die.java
@@ -3,7 +3,25 @@ package exercises2;
 public class Die {
 
 	public static void main(String[] args) {
-
+        
+		//My though is random would generate 0 (inclusive) to 1 (exclusive)
+		//So what I need to do is to * 6
+		//now the range of random would be 0 to 6 (exclusive)
+		//And I add 1 to the random, range becomes 1 to 7 (exclusive)
+		//Then I only want the integer part. Casting. use (int)
+		
+		int dice = ((int) ((Math.random() * 6) + 1));
+		System.out.println(dice);
+		
+		// I use for statement to check if there's anything wrong.		
+		//		for( int i=0; i<10000; i++) {
+		//			
+		//			
+		//			int dice = ((int) ((Math.random() * 6) + 1));
+		//
+		//			System.out.println(dice);
+		//			}
+		
 		//
 		// Use the random number generator
 		//    (Math.random())
