6dbbe3a792a6cfd36ce82b7eb315c5a296043572
alexteng <alexteng@192.168.1.8>
2018-08-29 19:03:16 -0500

    exercise2, RandomAverages done.

--- exercises/exercises2/RandomAverages.java
+++ exercises/exercises2/RandomAverages.java
@@ -1,8 +1,11 @@
 package exercises2;
 
+import cse131.ArgsProcessor;
+
 public class RandomAverages {
 
 	public static void main(String[] args) {
+
 		//
 		// Write a loop that computes the average
 		//     of 1, 2, ... 1000 doubles
@@ -12,13 +15,38 @@ public class RandomAverages {
 		//
 		// Your code goes below here.
 		
+		double sum = 0;
+		int i = 0;
+		while (i<1000) {
+			sum = sum + Math.random();
+			i = i + 1;
+			System.out.println("Average of sum is: " + sum / i);
+		}
+		
+		
+		
+		
+		
 		
 		
 		//
 		//  Some questions:
 		//    1) How does the average change as your loop progresses?
+		//      The average starts with some random number, as the i increases, the average tends to close to 0.5.
 		//    2) Can you write code in your loop that prints out the average only
 		//         every 100 iterations, instead of each iteration?
+		//Another way I'd try is:
+		//double sum = 0;
+		//ArgsProcessor ap = new ArgsProcessor(args);
+		//int input = ap.nextInt();
+		//for (int i=0; i < input; i++) {
+		//	sum = sum + Math.random();
+					
+		//}
+				
+		//double average = sum / input;
+		//System.out.println(average);
+		//       
 		//
 		
 	}
