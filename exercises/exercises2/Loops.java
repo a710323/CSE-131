49e83c947a553f5adca7a6bf4dc3b46575840477
alexteng <alexteng@192.168.1.8>
2018-08-29 20:30:08 -0500

    exercise2, Loops done

--- exercises/exercises2/Loops.java
+++ exercises/exercises2/Loops.java
@@ -43,18 +43,42 @@ public class Loops {
 		
 		// 0 to 9 inclusively
 		//    produces  0 1 2 3 4 5 6 7 8 9
+		int i = 0;
+		while (i<10) {
+			System.out.print(i + " ");
+			i = i +1;
+		}
+		
 		
 		
 		// 1 to 10 inclusively
 		//    produces 1 2 3 4 5 6 7 8 9 10
+		int j = 1;
+		System.out.println();//print the second loop at the next line
+
+		while (j<=10) {
+			System.out.print(j + " ");
+			j = j+1;
+		}
 		
 		
 		// 0 to 10, including 0, excluding 10
 		//    produces 0 1 2 3 4 5 6 7 8 9
+	    int k = 0;
+	    System.out.println();
+	    while (k<10) {
+	    	System.out.print(k + " ");
+	    	k = k+1;
+	    }
 		
-		
+		System.out.println();
+		int l = 0;
 		// like the one above, but jumping by 2
 		//    produces 0 2 4 6 8
+	    while (l<10) {
+	    	System.out.print(l + " ");
+	    	l = l+2;
+	    }
 	}
 
 }
