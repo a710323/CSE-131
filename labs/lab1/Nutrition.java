2f3311967e4d6c86f1a74a123fee0cb4e37e3370
alexteng <alexteng@192.168.1.8>
2018-08-29 22:51:18 -0500

    Lab1. first draft.

--- labs/lab1/Nutrition.java
+++ labs/lab1/Nutrition.java
@@ -12,12 +12,26 @@ public class Nutrition {
 		double statedCals = ap.nextDouble("How many is the calories stated on label? ");
 		
 		System.out.println(foodName + " has");
-		System.out.println(" " + carbs + " grams of carbohyrates = " + (double)Math.round((carbs*4)*100) / 100 + " Calories");
-		System.out.println(" " + fats + " grams of fats = " + (double)Math.round((fats*9)*100) / 100 + " Calories");
-		System.out.println(" " + protein + " grams of protein = " + (double) Math.round((protein*4)*100) / 100 + " Calories");
+		System.out.println("\t" + carbs + " grams of carbohyrates = " + (double)Math.round((carbs*4)*100) / 100 + " Calories");
+		System.out.println("\t" + fats + " grams of fats = " + (double)Math.round((fats*9)*100) / 100 + " Calories");
+		System.out.println("\t" + protein + " grams of protein = " + (double) Math.round((protein*4)*100) / 100 + " Calories");
 		System.out.println("This food is said to have " + (double) Math.round(statedCals*100) / 100 + " (available) Calories.");
 		double sumCals = carbs*4 + fats*9 + protein*4; 
 		System.out.println("With " + (double) Math.round((sumCals - statedCals)*100) / 100 + " (unavailable) Calories, this food has " + (double) Math.round(((sumCals - statedCals)/4)*100) / 100  + " grams of fiber");
+		System.out.println();
+		System.out.println("Approximately");
+		System.out.println("\t" + (double) Math.round(((carbs*4)/statedCals)*1000)/10 + "% of your food is carbohyrates");
+		System.out.println("\t" + (double) Math.round(((fats*9)/statedCals)*1000)/10 + "% of your food is fats");
+		System.out.println("\t" + (double) Math.round(((protein*4)/statedCals)*1000)/10 + "% of your food is protein");
+		System.out.println();
+		
+		boolean isLowCarb = ((double) Math.round(((carbs*4)/statedCals)*10)/10) <= 0.25;
+		boolean isLowFat = ((double) Math.round(((fats*9)/statedCals)*10)/10) <= 0.15;
+		System.out.println("This food is acceptible for a low-carb diet? \t" + isLowCarb);
+		System.out.println("This food is acceptible for a low-fat diet? \t" + isLowFat);
+		
+		boolean eatIt = Math.random() < 0.5;
+		System.out.println("By flip a coin, you should eat this food?\t" + eatIt );
 		
 	}
 
