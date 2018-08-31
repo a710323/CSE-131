17d7fdab982a6613fb5dee3b774c9623aa82c263
alexteng <alexteng@192.168.1.8>
2018-08-30 20:00:48 -0500

    Lab1 done.

--- labs/lab1/Nutrition.java
+++ labs/lab1/Nutrition.java
@@ -12,6 +12,15 @@ public class Nutrition {
 		double protein = ap.nextDouble("How much is the protein in this food?");
 		double statedCals = ap.nextDouble("How many is the calories stated on label? ");
 		
+//		double carbsCal = RoundTo2(carbs*4, 2);
+//		double fatsCal = RoundTo2(fats*9, 2);
+//		double proteinCal = RoundTo2(protein*4, 2);
+//		double carbsPercent = RoundTo1_Percentage(carbsCal/statedCals, 1);
+//		double fatsPercent = RoundTo1_Percentage(fatsCal/statedCals,1);
+//		double proteinPercent = RoundTo1_Percentage(proteinCal/statedCals, 1);
+//		
+//		System.out.println(carbsCal+ "\t"+ fatsCal + "\t" + proteinCal + "\t" + carbsPercent + "\t" + fatsPercent + "\t"  + proteinPercent );
+//		
 		//print out the info based on the instruction
 		//and do the calculation in print statement
 		//use Math.round() to round the number up to second decimal point by
@@ -49,6 +58,20 @@ public class Nutrition {
 		boolean eatIt = Math.random() < 0.5;
 		System.out.println("By flip a coin, you should eat this food?\t" + eatIt );
 		
+// Here I declare two methods to round the number, 
+		//first is to round the number to 2nd decimal point.
+		//second is to round the number to 1st decimal point and present is as a %
+		
+		
+//	public static double RoundTo2(double input, double decimal) {
+//		double decimal1 = Math.pow(10, decimal);
+//		double input1 = Math.round(input*decimal1) / Math.pow(10, decimal);
+//		return input1;
+//	}
+//	public static double RoundTo1_Percentage(double input, double decimal) {
+//		double decimal2 = Math.pow(10, decimal+2);
+//		double input2 = Math.round(input*decimal2) / Math.pow(10, decimal);
+//		return input2;
+//	}
 	}
-
 }
--- studios/studio1/Average.java
+++ studios/studio1/Average.java
@@ -4,9 +4,9 @@ import cse131.ArgsProcessor;
 public class Average {
 	public static void main(String[] args) {
 		ArgsProcessor input1 = new ArgsProcessor(args);
-		int Int1 = input1.nextInt("Enter the first of two integer you want to calculate the average.");
-		int Int2 = input1.nextInt("Enter the second of two integer you want to calculate the average.");
-		double average = ((double) Int1 + Int2) / 2;
+		int n1 = input1.nextInt("Enter the first of two integer you want to calculate the average.");
+		int n2 = input1.nextInt("Enter the second of two integer you want to calculate the average.");
+		double average = ((double) n1 + n2) / 2;
 		System.out.println("The average of the two integer is: " + average);
 	}
 }
