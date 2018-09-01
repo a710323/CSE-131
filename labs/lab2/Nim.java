151c48ecdfdf4cd9f785cf209e0cc4e2df4a8a2c
alexteng <alexteng@192.168.1.8>
2018-08-31 20:29:13 -0500

    Lab2 done

---
+++ labs/lab2/Nim.java
@@ -0,0 +1,60 @@
+package lab2;
+
+import cse131.ArgsProcessor;
+
+public class Nim {
+	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int whichMoves = ap.nextInt("First move or Second move? Enter 1 or 2");
+		
+		while (whichMoves > 2 || whichMoves <= 0) {
+			whichMoves = ap.nextInt("First move or Second move? Enter 1 or 2");
+		}
+		int numSticks = ap.nextInt("Enter the number of sticks at the start of the game");
+		int i = 0;
+		if (whichMoves == 1) {
+			int humanMoves = ap.nextInt("Enter the number of sticks you want to move out, 1 or 2");
+			while (humanMoves > 2 || humanMoves <= 0 || humanMoves > numSticks) {
+				humanMoves = ap.nextInt("Enter the number of sticks you want to move out, 1 or 2");
+			}
+			System.out.println("Human starts");
+			System.out.println("Round "+ i + " " + numSticks + " sticks at start, human took " + humanMoves + " sticks, so " + (numSticks-humanMoves) + " sticks remain.");
+
+			numSticks = numSticks - humanMoves;
+			i = i + 1;
+		}
+		else {
+			System.out.println("Computer starts");
+		}
+		while(numSticks > 0){
+			int compMoves = (int)(Math.random()*2 + 1);
+			while (compMoves > numSticks) {
+				compMoves = (int)(Math.random()*2 + 1);
+			}
+			System.out.println("Round "+ i + " " + numSticks + " sticks at start, computer took " + compMoves + " sticks, so " + (numSticks-compMoves) + " sticks remain.") ;
+			numSticks = numSticks - compMoves;
+			if (numSticks == 0) {
+				System.out.println("Computer wins");
+			}
+			i = i + 1;
+			int humanMoves = ap.nextInt("Enter the number of sticks you want to move out, 1 or 2");
+			while (humanMoves > 2 || humanMoves <= 0 || humanMoves > numSticks) {
+				humanMoves = ap.nextInt("Enter the number of sticks you want to move out, 1 or 2");
+			}
+			System.out.println("Round "+ i + " " + numSticks + " sticks at start, human took " + humanMoves + " sticks, so " + (numSticks-humanMoves) + " sticks remain.");
+
+			numSticks = numSticks - humanMoves;
+			if(numSticks == 0) {
+				System.out.println("Human wins");
+			}
+			i = i + 1;
+		}
+		
+			
+			
+		
+		
+		
+	}
+
+}
