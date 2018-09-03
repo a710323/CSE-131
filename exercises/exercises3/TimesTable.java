ed4a1da5084c071e07d9056533caf9650b71e7fb
alexteng <alexteng@192.168.1.8>
2018-09-03 10:09:20 -0500

    exercise3, TimesTable done

--- exercises/exercises3/TimesTable.java
+++ exercises/exercises3/TimesTable.java
@@ -8,9 +8,29 @@ public class TimesTable {
 		ArgsProcessor ap = new ArgsProcessor(args);
 		int N = ap.nextInt("Max value for table?");
 		
-		//  Table should include rows and columns for 0...N  
+				//  Table should include rows and columns for 0...N  
 		//     mkaing N+1 rows and columns
-		
+		int[][] table = new int[N+1][N+1];
+		System.out.print("     ");
+		for (int i=0; i<N+1;i++) {
+			System.out.print(i+ "  ");
+		}
+		System.out.println();
+		System.out.println("------------------------------------");
+		for (int i=0; i < N+1; i++) {
+			System.out.print(i + " | ");
+			for (int j=0; j < N+1; j++) {
+				table[i][j] = i * j;
+				int entry = table[i][j];
+				if (entry < 10) {
+					System.out.print(" " + table[i][j]+" ");
+				}
+				else {
+					System.out.print(table[i][j]+ " ");
+				}
+			
+			}
+			System.out.println();
+		}	
 	}
-
 }
