b7babcedabb377ee3cf66ccccde6cce06650362a
alexteng <alexteng@192.168.1.11>
2018-09-05 22:08:54 -0500

    Exercise2, all done.

--- exercises/exercises2/RandomAverages.java
+++ exercises/exercises2/RandomAverages.java
@@ -21,8 +21,8 @@ public class RandomAverages {
 			sum = sum + Math.random();
 			i = i + 1;
 			System.out.println("Average of sum is: " + sum / i);
+
 		}
-		
 		
 		
 		
@@ -32,18 +32,21 @@ public class RandomAverages {
 		//
 		//  Some questions:
 		//    1) How does the average change as your loop progresses?
-		//      The average starts with some random number, as the i increases, the average tends to close to 0.5.
+		//       Answer:
+		//       The average starts with some random number, as the i increases, the average tends to close to 0.5.
 		//    2) Can you write code in your loop that prints out the average only
 		//         every 100 iterations, instead of each iteration?
+		//       Answer:
+		//       I could put the System.out.println("Average of sum is: " + sum /i); outside the loop
+	
+		
 		//Another way I'd try is:
 		//double sum = 0;
 		//ArgsProcessor ap = new ArgsProcessor(args);
 		//int input = ap.nextInt();
 		//for (int i=0; i < input; i++) {
-		//	sum = sum + Math.random();
-					
-		//}
-				
+		//	sum = sum + Math.random();			
+		//}		
 		//double average = sum / input;
 		//System.out.println(average);
 		//       
