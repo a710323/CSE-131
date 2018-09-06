400158c77e6e3fd65627c2262003dcd4c5b93bb8
alexteng <alexteng@192.168.1.11>
2018-09-05 22:09:52 -0500

    TimesTable, done

--- exercises/exercises3/TimesTable.java
+++ exercises/exercises3/TimesTable.java
@@ -1,5 +1,7 @@
 package exercises3;
 
+import java.util.Arrays;
+
 import cse131.ArgsProcessor;
 
 public class TimesTable {
@@ -10,6 +12,19 @@ public class TimesTable {
 		
 		//  Table should include rows and columns for 0...N  
 		//     mkaing N+1 rows and columns
+		int[][] index = new int[N+1][N+1];
+		for(int i=0; i < N+1; i++) {
+			for(int j=0; j< N+1; j++) {
+				index[i][j] = i * j;
+			}
+		}
+		
+		for (int i=0; i < N+1; i++) {
+			for (int j=0; j < N+1; j++) {
+				System.out.print(index[i][j] + " ");
+			}
+			System.out.println();
+		}
 		
 	}
 
