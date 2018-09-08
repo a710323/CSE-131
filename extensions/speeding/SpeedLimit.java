3c06ee723a1db46908737fef73b1c075460e9721
alexteng <alexteng@10.37.129.2>
2018-09-08 13:45:37 -0500

    Lab2 modification and Extension: SpeedLimit done.

---
+++ extensions/speeding/SpeedLimit.java
@@ -0,0 +1,20 @@
+package speeding;
+
+import cse131.ArgsProcessor;
+
+public class SpeedLimit {
+	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int speedLimit = ap.nextInt("Enter the speed limit");
+		int speed = ap.nextInt("What's the speed of vehicle");
+		int offLimit = ((speed - speedLimit) > 0) ? speed - speedLimit: 0;
+		int firstFine = (speed > (speedLimit+10)) ? 50 : 0;
+		int secondFine = (speed - (speedLimit+10) > 0) ? (speed - (speedLimit+10))*10 : 0;
+		
+		System.out.println("You reported a speed of " + speed + " MPH for a speed limit of " + speedLimit + " MPH.");
+		System.out.println("You went " + offLimit + " over the speed limit.");
+		System.out.println("Your fine is $" + (firstFine + secondFine));
+		
+	}
+
+}
--- labs/lab2/Nim.java
+++ labs/lab2/Nim.java
@@ -5,9 +5,10 @@ import cse131.ArgsProcessor;
 public class Nim {
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
+
 		int whichMoves = ap.nextInt("First move or Second move? Enter 1 or 2");
-		
-		while (whichMoves > 2 || whichMoves <= 0) {
+
+		while (whichMoves != 2 && whichMoves != 1) {
 			whichMoves = ap.nextInt("First move or Second move? Enter 1 or 2");
 		}
 		int numSticks = ap.nextInt("Enter the number of sticks at the start of the game");
@@ -18,9 +19,12 @@ public class Nim {
 				humanMoves = ap.nextInt("Enter the number of sticks you want to move out, 1 or 2");
 			}
 			System.out.println("Human starts");
-			System.out.println("Round "+ i + " " + numSticks + " sticks at start, human took " + humanMoves + " sticks, so " + (numSticks-humanMoves) + " sticks remain.");
+			System.out.println("Round "+ i + " " + numSticks + " sticks at start, human took " + humanMoves + ", so " + (numSticks-humanMoves) + " sticks remain.");
 
 			numSticks = numSticks - humanMoves;
+			if (numSticks == 0 ) {
+				System.out.println("human wins");
+			}
 			i = i + 1;
 		}
 		else {
@@ -31,26 +35,27 @@ public class Nim {
 			while (compMoves > numSticks) {
 				compMoves = (int)(Math.random()*2 + 1);
 			}
-			System.out.println("Round "+ i + " " + numSticks + " sticks at start, computer took " + compMoves + " sticks, so " + (numSticks-compMoves) + " sticks remain.") ;
+			System.out.println("Round "+ i + " " + numSticks + " sticks at start, computer took " + compMoves + ", so " + (numSticks-compMoves) + " sticks remain.") ;
 			numSticks = numSticks - compMoves;
 			if (numSticks == 0) {
 				System.out.println("Computer wins");
 			}
 			i = i + 1;
-			int humanMoves = ap.nextInt("Enter the number of sticks you want to move out, 1 or 2");
-			while (humanMoves > 2 || humanMoves <= 0 || humanMoves > numSticks) {
-				humanMoves = ap.nextInt("Enter the number of sticks you want to move out, 1 or 2");
-			}
-			System.out.println("Round "+ i + " " + numSticks + " sticks at start, human took " + humanMoves + " sticks, so " + (numSticks-humanMoves) + " sticks remain.");
+			if (numSticks > 0) {
+				int humanMoves = ap.nextInt("Enter the number of sticks you want to move out, 1 or 2");
+				while (humanMoves > 2 || humanMoves <= 0 || humanMoves > numSticks) {
+					humanMoves = ap.nextInt("Enter the number of sticks you want to move out, 1 or 2");
+				}
+				System.out.println("Round "+ i + " " + numSticks + " sticks at start, human took " + humanMoves + ", so " + (numSticks-humanMoves) + " sticks remain.");
 
-			numSticks = numSticks - humanMoves;
-			if(numSticks == 0) {
-				System.out.println("Human wins");
+				numSticks = numSticks - humanMoves;
+				if(numSticks == 0) {
+					System.out.println("Human wins");
+				}
+				i = i + 1;
 			}
-			i = i + 1;
 		}
-	
-		
+
 	} 
 
 }
