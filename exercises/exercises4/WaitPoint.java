b87fc629ff8fc615db772f84315f8a8157f24689
alexteng <alexteng@192.168.1.11>
2018-09-17 17:19:11 -0500

    Exercise 4.50 done

--- exercises/exercises4/WaitPoint.java
+++ exercises/exercises4/WaitPoint.java
@@ -5,21 +5,31 @@ import sedgewick.StdDraw;
 public class WaitPoint {
 
 	public static void main(String[] args) {
+
 		// wait for the mouse to be pressed and released
-
+		while (!StdDraw.mousePressed()) {
+			StdDraw.pause(100);
+		}
 		// here, the mouse has been pressed
-
+		System.out.println("Pressed");
+		while (StdDraw.mousePressed()) {
+			StdDraw.pause(100);
+		}
 		// here the mouse is released
-		
-		
+		System.out.println("Released");
+
 		// draw a point at the location of the mouse
-		
-		
-		// here, a q has been typed
-		
-		
+		StdDraw.setPenRadius(0.1);
+		StdDraw.point(StdDraw.mouseX(), StdDraw.mouseY());
+
+		char got = ' ';
+		while (got !='q') {	
+			// here, a q has been typed
+			while (!StdDraw.hasNextKeyTyped()) {
+				StdDraw.pause(100);
+			}
+			got = StdDraw.nextKeyTyped();
+		}
 		StdDraw.text(0.5, 0.5, "Farewell!");
-
 	}
-
 }
