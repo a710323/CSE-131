cb9327cf915d20188192d5691c5c984f2c2f3874
alexteng <alexteng@toumeiabookpuro.dhcp.wustl.edu>
2018-09-18 11:15:05 -0500

    Lab 3 done.

--- labs/lab3/Dice.java
+++ labs/lab3/Dice.java
@@ -7,7 +7,7 @@ public class Dice {
 		ArgsProcessor ap = new ArgsProcessor(args);
 		int numDice = ap.nextInt("How many dice?");
 		int numSimu = ap.nextInt("How many simulations?");
-		int[] stored = new int[numDice];
+		int[] arr = new int[numDice];
 		int numSame = 0;
 		int[][] sumAndTimes = new int[numDice*6-numDice+1][2];
 
@@ -16,23 +16,23 @@ public class Dice {
 		}
 
 		for(int j=0; j<numSimu; j++) { //number of the simulation
-			int total = 0;
+			int sum = 0;
 			boolean same = true; //initialize a boolean to check if each dice in a simulation is the same
 			for (int i=0; i < numDice; i++) { //number of the dice in each simulation
 				int rollDice = (int) (Math.random()*6+1); //generate the value from 1 to 6
-				stored[i] = rollDice; //store the value to the array stored
+				arr[i] = rollDice; //store the value to the array stored
 				for (int k=1; k<=i;k++) { //check if the value of each dice is the same
-					if(stored[0] != stored[k]) {
+					if(arr[0] != arr[k]) {
 						same = false; 
 					}
 				}
-				total = rollDice + total;
+				sum = rollDice + sum;
 			}
 			if(same == true) {
 				numSame++;
 			}
 			for(int l=0; l<sumAndTimes.length; l++) {
-				if (sumAndTimes[l][0] == total) {
+				if (sumAndTimes[l][0] == sum) {
 					sumAndTimes[l][1] = sumAndTimes[l][1]+1;
 				}
 			}
@@ -44,7 +44,6 @@ public class Dice {
 				System.out.println(sumAndTimes[o][0] + "\t\t"+ sumAndTimes[o][1] +"\t\t\t" + (double)Math.round(((double)sumAndTimes[o][1]/numSimu)*1000)/10 +"%");
 			}
 		}
-		
 		System.out.println();
 		System.out.println("The ratio of all dice are the same is: " + (double)Math.round((numSame/ (double)numSimu)*1000)/10 + "%");
 	}
