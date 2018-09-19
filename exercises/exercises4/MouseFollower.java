b4272a89d362139ddf8f55f4384b78e769d1a57b
alexteng <alexteng@toumeiabookpuro.dhcp.wustl.edu>
2018-09-18 21:42:48 -0500

    exercise 4.60 done

--- exercises/exercises4/MouseFollower.java
+++ exercises/exercises4/MouseFollower.java
@@ -8,15 +8,34 @@ import sedgewick.StdDraw;
 public class MouseFollower {
 
 	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int N  = ap.nextInt("How many history");
+		double[] X = new double[N];
+		double[] Y = new double[N];
+		int cur = 0;
 		
-		
+
+		StdDraw.setPenColor(Color.blue);
+
 		while (true) {
 			StdDraw.clear();
 			//
 			// Render one frame of your animation below here
 			//
-
+			double x = StdDraw.mouseX();
+			double y = StdDraw.mouseY();
 			
+			X[cur] = x;
+			Y[cur] = y;
+			cur = cur + 1;
+			if (cur >= X.length) {
+				cur = 0;
+			}
+			int history = cur;
+					
+			StdDraw.filledCircle(X[history], Y[history], 0.02);
+
+
 			//  
 			// End of your frame
 			//
