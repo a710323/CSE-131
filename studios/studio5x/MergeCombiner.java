ba836e7ffdb0e0d4e4b0b7d27766e7bb5d804ad4
alexteng <alexteng@alexteng-mbp.dhcp.wustl.edu>
2018-10-11 12:05:59 -0500

    Studio 5X done.

--- studios/studio5x/MergeCombiner.java
+++ studios/studio5x/MergeCombiner.java
@@ -24,6 +24,37 @@ public class MergeCombiner {
 	 * @return a new array which contains the contents of both a and b, sorted.
 	 */
 	public static String[] createMergeCombinedArray(String[] a, String[] b) {
-		throw new NotYetImplementedException("delete this line of code and implement this method.");
+		String[] input = new String[a.length+b.length];
+		int i =0, j = 0, z=0;
+		while(i<a.length && j<b.length) {
+
+			if(Strings.isLessThan(a[i], b[j])) {
+				input[z]=a[i];
+				z++; i++;
+			}
+			else if(Strings.isLessThan(b[j], a[i])) {
+				input[z]=b[j];
+				z++; j++;
+			}
+			else {
+				input[z] = a[i];
+				z++;
+				input[z] = b[j];
+				z++;
+				i++;
+				j++;
+			}
+		}
+		while(j<b.length) {
+			input[z] = b[j];
+			j++;
+			z++;
+		}
+		while(i<a.length) {
+			input[z] = a[i];
+			i++;
+			z++;
+		}
+		return input;
 	}
 }
