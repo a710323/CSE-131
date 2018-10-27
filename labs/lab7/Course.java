ad31b8b75ae31d79c9b2c353edb0b280ed164828
alexteng <alexteng@192.168.1.11>
2018-10-27 15:53:34 -0500

    Lab 7 done.

---
+++ labs/lab7/Course.java
@@ -0,0 +1,68 @@
+package lab7;
+
+public class Course {
+	private String name;
+	private int credits;
+	private int capacity;
+	private int numSeats;
+	private Student[] roster;
+
+	public Course(String name, int credits, int seats) {
+		this.name = name;
+		this.credits = credits;
+		this.numSeats = seats;
+		this.capacity = seats;
+		this.roster = new Student[seats];
+	}
+	public int getCredits() {
+		return this.credits;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public int getCapacity() {
+		return this.capacity;
+	}
+	public int getSeatsRemaining() {
+		return this.numSeats;
+	}
+	public boolean addStudent(Student s) {
+		if (this.getSeatsRemaining() <= 0) {
+			return false;
+		}
+		for (int i = 0; i < this.roster.length; i++) {
+			if (this.roster[i] == null) {
+				this.roster[i] = s;
+				this.numSeats = this.numSeats - 1;
+				return true;
+			}
+			if (this.roster[i].compare(s)) {
+				return false;
+			}
+		}
+		return false;
+	}
+	public String generateRoster() {
+		String ans = "";
+		for(int i = 0; i < this.roster.length; i++) {
+			if(this.roster[i] != null) {
+				ans = ans + " " + this.roster[i].getFullName();
+			}
+		}
+		return ans;
+	}
+	public double calculateAverageGPA() {
+		double ans = 0;
+		for(int i = 0; i < this.roster.length; i++) {
+			if(this.roster[i] == null) {
+				return ans;
+			}
+			ans = ans*(i) + this.roster[i].calculateGradePointAverage();
+			ans = ans/(i+1);
+		}
+		return ans;
+	}
+	public String toString() {
+		return this.getName() + " " + this.getCredits();
+	}
+}
--- labs/lab7/Student.java
+++ labs/lab7/Student.java
@@ -7,6 +7,7 @@ public class Student {
 	private int attemptedCredit;
 	private int passingCredit;
 	private double point;
+	private double gpa;
 	private double bearBucks;
 
 	public Student(String firstName, String lastName, int id) {
@@ -14,9 +15,22 @@ public class Student {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.id = id;
-		point = 0.0;
+		this.point = 0.0;
 		this.attemptedCredit = 0;
 		this.bearBucks = 0;
+		this.gpa = Double.NaN;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+	public boolean compare(Student s2) {
+		if(this.getFullName().compareTo(s2.getFullName()) == 0) {
+			if(this.getId() == s2.getId()) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public String getFullName() {
@@ -26,18 +40,24 @@ public class Student {
 		return id;
 	}
 	public void submitGrade(double grade, int credits) {
-		if (grade < 1.7) {
-			this.attemptedCredit += credits;
-			this.passingCredit += 0;
-		}
-		else {
-			this.attemptedCredit += credits;
-			this.passingCredit += credits;
-		}
 		double tempGPA = (grade * credits + this.point*this.attemptedCredit)
 				/(this.attemptedCredit+credits);
 		this.point = tempGPA;
-
+		this.gpa = tempGPA;
+		if (grade > 4.0 || grade < 0) {
+			return;
+		}
+		else {
+			if (grade < 1.7) {
+				this.attemptedCredit += credits;
+				this.passingCredit += 0;
+			}
+			else {
+				this.attemptedCredit += credits;
+				this.passingCredit += credits;
+			}
+			
+		}
 	}
 	public int getTotalAttemptedCredits() {
 		return this.attemptedCredit;
@@ -46,11 +66,85 @@ public class Student {
 		return this.passingCredit;
 	}
 	public double calculateGradePointAverage() {
-		if (this.point <=4.0 && this.point >=0) {
-			return this.point;
-		}
-		return 0.0;
+		return this.gpa;
 	}
+	public String getClassStanding() {
+		if (passingCredit < 30) {
+			return "FirstYear";
+		}
+		else if(passingCredit>=30 && passingCredit <60) {
+			return "Sophomore";
+		}
+		else if(passingCredit>=60 && passingCredit < 90) {
+			return "Junior";
+		}
+		else {
+			return "Senior";
+		}
+	}
+	public boolean isEligibleForPhiBetaKappa() {
+		if (this.gpa >= 3.6) {
+			if (this.passingCredit >= 98) {
+				return true;
+			}
+			if(this.passingCredit >= 75) {
+				if (this.gpa >= 3.8) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		Student Alex = new Student("Alex", "Teng", 1);
+		Alex.submitGrade(3.0, 3);
+		double x = Alex.calculateGradePointAverage();
 
-
+		Alex.submitGrade(3.9, 4);
+		double y = Alex.calculateGradePointAverage();
+		System.out.println(x);
+		System.out.println(y);
+	}
+	
+	public void depositBearBucks(double amount) {
+		this.bearBucks += amount;
+	}
+	public void deductBearBucks(double amount) {
+		this.bearBucks -= amount;
+	}
+	public double getBearBucksBalance() {
+		return this.bearBucks;
+	}
+	public double cashOutBearBucks() {
+		if (this.bearBucks <= 10.0) {
+			this.bearBucks = 0;
+			return 0;
+		}
+		else {
+			double ans = this.bearBucks - 10;
+			this.bearBucks = 0;
+			return ans;
+			
+		}
+	}
+	public String concatLastName(Student otherParent, boolean isHyphenated) {
+		if (isHyphenated == true) {
+			return this.lastName+"-"+otherParent.lastName;
+		}
+		else {
+			return this.lastName;
+		}
+	}
+	
+	public Student createLegacy(String firstName, Student otherParent, boolean isHyphenated, int id) {
+		Student child = new Student(firstName, concatLastName(otherParent, isHyphenated), id);
+		child.bearBucks = this.cashOutBearBucks() + otherParent.cashOutBearBucks();
+		return child;
+	}
+	public String toString() {
+		return this.getFullName() + " " + this.id;
+	}
+	
+	
 }
