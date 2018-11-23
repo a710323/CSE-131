29e6d0cf2d038879ef4a8e6677fe347aa2341d1e
alexteng <alexteng@alexteng-mbp.dhcp.wustl.edu>
2018-11-22 18:23:02 -0600

    Extension Music done.

--- extensions/chords/music/Triad.java
+++ extensions/chords/music/Triad.java
@@ -2,14 +2,21 @@ package chords.music;
 
 public class Triad {
 	
-	
+	private SingleTone base; 
+	private SingleTone twoDiatonic;
+	private SingleTone fourDiatonic;
 	/**
 	 * Diatonic triad starting at the specified note
 	 * @param ds  Scale for the triad
 	 * @param root  Starting note of the triad
 	 */
 	public Triad(DiatonicScale ds, int root) {
-		//FIXME
+		double a = ds.getPitch(root).getHertz();
+		double b = ds.getPitch(root + 2).getHertz();
+		double c = ds.getPitch(root + 4).getHertz();
+		this.base = new SingleTone(a);
+		this.twoDiatonic = new SingleTone(b);
+		this.fourDiatonic = new SingleTone(c);
 	}
 	
 	/**
@@ -17,7 +24,11 @@ public class Triad {
 	 * @return an array with the root, middle, top notes of this triad
 	 */
 	public SingleTone[] getTones() {
-		return null;  // FIXME
+		SingleTone[] copy = new SingleTone[3];
+		copy[0] = this.base;
+		copy[1] = this.twoDiatonic;
+		copy[2] = this.fourDiatonic;
+		return copy;
 	}
 	
 	public String toString() {
@@ -32,7 +43,10 @@ public class Triad {
 	 * @return samples of this triad at the specified amplitude and duration
 	 */
 	public Samples getSamples(double amplitude, double duration) {
-		return null;  // FIXME
+		Samples a = this.base.getSamples(amplitude/3, duration);
+		Samples b = this.twoDiatonic.getSamples(amplitude/3, duration);
+		Samples c = this.fourDiatonic.getSamples(amplitude/3, duration);
+		Samples combined = a.combine(b).combine(c);
+		return combined;
 	}
-
 }
